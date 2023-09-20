b1a683389e91bf493ae7b0d2fc9fac09bfdc01ea
jvcarlosjose <30812890+jvcarlosjose@users.noreply.github.com>
2023-09-20 07:58:01 -0500

    adjust codes

--- app/src/main/java/com/greenshark/beatbox/MainActivity.kt
+++ app/src/main/java/com/greenshark/beatbox/MainActivity.kt
@@ -56,7 +56,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkReadMediaAudioPermission() {
-        val permissionToCheck = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        val permissionToCheck = if (Build.VERSION.SDK_INT >=
+            Build.VERSION_CODES.TIRAMISU) {
             android.Manifest.permission.READ_MEDIA_AUDIO
         } else {
             android.Manifest.permission.READ_EXTERNAL_STORAGE
